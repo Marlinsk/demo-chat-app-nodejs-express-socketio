2fc389b4fbef8582743ef973bfb354c6b030efe1
Marlon <Marlonnlmx@outlook.com>
2023-11-05 16:04:52 -0300

    feat: tests implemented

--- server.js
+++ server.js
@@ -42,25 +42,25 @@ app.get('/messages/:user', (req, res) => {
 });
 
 app.post('/messages', async (req, res) => {
-  try{
+  try {
     var message = new Message(req.body);
 
-    var savedMessage = await message.save()
-      console.log('saved');
+    var savedMessage = await message.save();
+    console.log('saved');
 
-    var censored = await Message.findOne({message:'badword'});
-      if(censored)
-        await Message.remove({_id: censored.id})
-      else
-        io.emit('message', req.body);
-      res.sendStatus(200);
-  }
-  catch (error){
+    var censored = await Message.findOne({ message: 'badword' });
+    if (censored)
+      await Message.remove({ _id: censored.id });
+    else
+      io.emit('message', req.body);
+
+    res.json({ message: 'Message Posted' });
+
+  } catch (error) {
     res.sendStatus(500);
-    return console.log('error',error);
-  }
-  finally{
-    console.log('Message Posted')
+    return console.log('error', error);
+  } finally {
+    console.log('Message Posted');
   }
 
 })
@@ -74,6 +74,8 @@ mongoose.connect(process.env.DATABASE_URL);
 mongoose.connection.on('open', () => {
   console.log('mongodb connected');
 });
+
+module.exports = http;
 
 var server = http.listen(3000, () => {
   console.log('server is running on port', server.address().port);
